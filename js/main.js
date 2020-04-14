99370587756a9797bdb58c3d0b22897c37787afb
NightScript <maorninja@outlook.com>
2020-04-13 23:58:43 -0700

    New things

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 document.getElementById('sidebarCollapse').addEventListener('click', () => document.getElementById('sidebar').classList.toggle('active'));
+
+mybutton = document.getElementById("bttbtn");
+mybutton.classList.toggle('fade');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? mybutton.style.opacity = 1 : mybutton.style.opacity = 0;
