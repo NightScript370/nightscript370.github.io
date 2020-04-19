00a8c2faf106c6dc942bf6e61f5f096a2f92d172
NightScript <maorninja@outlook.com>
2020-04-18 22:32:13 -0700

    Move gaming to new blog entry, implement social links with cards and fix web manifest

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 //document.designMode = 'on';
 document.getElementById('sidebarCollapse').addEventListener('click', () => document.getElementById('sidebar').classList.toggle('active'));
 
+/*Scroll to top button */
 mybutton = document.getElementById("bttbtn");
 mybutton.classList.toggle('fade');
 
@@ -12,3 +13,42 @@ function scrollUp() {
 	document.documentElement.scrollTop = 0;
 	event.preventDefault()
 }
+
+/* Share Links button for cards */
+
+const sharedLinks = [
+	{
+		"title": "FaceBook",
+		"icon": "facebook-official",
+		"baselink": "https://www.facebook.com/sharer/sharer.php?u="
+	},
+	{
+		"title": "Twitter",
+		"icon": "twitter",
+		"baselink": "https://twitter.com/intent/tweet?text=",
+		"text": 'Look at this cool blog entry I found on NightScript Domain - "$title$": '
+	}
+]
+
+function replaceNoShare(cards) {
+	if (!cards.length)
+		return;
+
+	let entrylink, entrytitle, socialLinksHTML;
+	for (const int of cards) {
+		entrylink = int.parentElement.firstElementChild.getAttribute("href");
+		entrytitle = int.parentElement.firstElementChild.innerHTML;
+
+		socialLinksHTML = '<div class="text-center cardShareLinks">';
+		for (let socialEntry of sharedLinks) {
+			socialLinksHTML += `<a class="card-link" href="${
+					socialEntry.baselink
+						+ (socialEntry.text ? encodeURI(socialEntry.text.replace("$title$", entrytitle)) : '')
+						+ encodeURI(window.location.protocol + "//" + window.location.hostname + entrylink)
+				}"><i class="fa fa-${socialEntry.icon} fa-2x"></i></a>`;
+		}
+		socialLinksHTML += "</div>";
+		int.parentElement.insertAdjacentHTML('beforeend', socialLinksHTML)
+	}
+}
+replaceNoShare(document.getElementsByClassName('noShare'));
