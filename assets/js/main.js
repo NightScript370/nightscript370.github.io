73243dddf7206116cd744c751d0e900289ba34a6
NightScript <maorninja@outlook.com>
2020-04-19 18:21:14 -0700

    Implement tooltipping for social links

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,14 +69,20 @@ function replaceCardNoShare(cards) {
 
 		socialLinksHTML = '<div class="text-center cardShareLinks">';
 		for (let socialEntry of sharedLinks.filter(entry => entry.cardview)) {
-			socialLinksHTML += `<a class="card-link" href="${
-					socialEntry.baselink.replace("''$TEXT$''", (socialEntry.separateText ? encodeURI(socialEntry.separateText.replace("$title$", entrytitle)) : ''))
-						+ (socialEntry.text ? encodeURI(socialEntry.text.replace("$title$", entrytitle)) : '')
-						+ encodeURI(window.location.protocol + "//" + window.location.hostname + entrylink)
-				}"><i class="fa fa-${socialEntry.icon} fa-2x"></i></a>`;
+			socialLinksHTML += `<a
+					class="card-link"
+					data-tippy-content="Share this on ${socialEntry.title}!"
+					href="${
+						socialEntry.baselink.replace("''$TEXT$''", (socialEntry.separateText ? encodeURI(socialEntry.separateText.replace("$title$", entrytitle)) : ''))
+							+ (socialEntry.text ? encodeURI(socialEntry.text.replace("$title$", entrytitle)) : '')
+							+ encodeURI(window.location.protocol + "//" + window.location.hostname + entrylink)
+					}"
+				><i class="fa fa-${socialEntry.icon} fa-2x"></i></a>`;
 		}
 		socialLinksHTML += "</div>";
 		int.parentElement.insertAdjacentHTML('beforeend', socialLinksHTML)
 	}
 }
 replaceCardNoShare(document.getElementsByClassName('noShare'));
+
+tippy('[data-tippy-content]');
