b354c99f7402b81bfa79ee29b908847cc19e65ca
NightScript <maorninja@outlook.com>
2020-05-18 14:35:41 -0700

    Fix back to top button inserting # on JavaScript users

--- assets/js/scrollup.js
+++ assets/js/scrollup.js
@@ -1,13 +1,13 @@
 /*Scroll to top button */
 const mybutton = document.getElementById("bttbtn");
 mybutton.classList.toggle('fade');
+mybutton.setAttribute('href', '');
+mybutton.addEventListener('click', () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+	event.preventDefault()
+});
 
 document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? mybutton.style.opacity = 1 : mybutton.style.opacity = 0;
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = () => document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? mybutton.style.opacity = 1 : mybutton.style.opacity = 0;
-
-function scrollUp() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-	event.preventDefault()
-}
