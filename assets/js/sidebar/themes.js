5db7f9bd4bb56d6d19e67054052f615e24b49649
NightScript <maorninja@outlook.com>
2020-05-27 12:23:12 -0700

    Add discord dark theme.
    Credits to tee, Samplasion and wrath for giving me feedback

--- assets/js/sidebar/themes.js
+++ assets/js/sidebar/themes.js
@@ -29,6 +29,20 @@ const themes = {
 		'--sidebar-link': 'rgba(0, 0, 0, 0.6)',
 		'--sidebar-link-active': 'black',
 		'--shadow-color': '#666'
+	},
+	'discord': {
+		'--main-bg-color': '#202225',
+		'--menu-element': '#2F3136',
+		'--darker-menu-element': '#1E2025',
+		'--menu-element-text-color': 'white',
+		'--lighter-menu-element': '#3F4247',
+		'--gradient-top': '#202225',
+		'--text-color': 'white',
+		'--background': "#36393F center top/cover no-repeat",
+		'--background-filter': 'initial',
+		'--sidebar-link': 'rgba(255, 255, 255, 0.6)',
+		'--sidebar-link-active': 'white',
+		'--shadow-color': 'transparent'
 	}
 }
 
