a1dc4c948704e29f9a156b22478fcfb7b6247ca0
NightScript <maorninja@outlook.com>
2020-06-02 12:34:06 -0700

    Make lighter proxim for the elements that aren't hovered

--- assets/js/proximity.js
+++ assets/js/proximity.js
@@ -7,9 +7,9 @@ class ProximityManager {
 		let rect = proxim.getBoundingClientRect();
 		let x = pos.x - rect.left; // offset from corner
 		let y = pos.y - rect.top;
-		let color = getComputedStyle(document.documentElement).getPropertyValue("--hover-color");
+		let color = getComputedStyle(document.documentElement).getPropertyValue(proxim.matches(":hover") ? "--hover-color" : "--light-hover-color");
 
-		proxim.style.background = (proxim.matches(":hover") ? `radial-gradient( circle at ${x}px ${y}px, ${color} 0%, transparent ${this.radius}px)` : null);
+		proxim.style.background = `radial-gradient( circle at ${x}px ${y}px, ${color} 0%, transparent ${this.radius}px)`;
 	}
 }
 
