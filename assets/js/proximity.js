693d38f4d8afe7d67a6ae5b4f448792b8434cc4c
NightScript <maorninja@outlook.com>
2020-06-04 10:05:27 -0700

    Clean Up proximity code

--- assets/js/proximity.js
+++ assets/js/proximity.js
@@ -1,25 +1,27 @@
 class ProximityManager {
-	constructor() {
+	constructor(element) {
+		this.proxim = element;
 		this.radius = 80;
 	}
 
-	effect(proxim, pos) {
-		let rect = proxim.getBoundingClientRect();
+	effect(pos) {
+		let rect = this.proxim.getBoundingClientRect();
 		let x = pos.x - rect.left; // offset from corner
 		let y = pos.y - rect.top;
 		let color = getComputedStyle(document.documentElement).getPropertyValue("--hover-color");
 		let lighterColor = getComputedStyle(document.documentElement).getPropertyValue("--light-hover-color");
 
-		if (proxim.matches(":hover"))
-			proxim.style.background = lighterColor + ` radial-gradient( circle at ${x}px ${y}px, ${color} 0%, transparent ${this.radius}px)`;
+		if (this.proxim.matches(":hover"))
+			this.proxim.style.background = lighterColor + ` radial-gradient( circle at ${x}px ${y}px, ${color} 0%, transparent ${this.radius}px)`;
 		else
-			proxim.style.background = `radial-gradient( circle at ${x}px ${y}px, ${lighterColor} 0%, transparent ${this.radius}px)`;
+			this.proxim.style.background = `radial-gradient( circle at ${x}px ${y}px, ${lighterColor} 0%, transparent ${this.radius}px)`;
 	}
 }
 
 const listener = (elementArray) => {
-	const proximityManager = new ProximityManager();
-
-	window.addEventListener("mousemove", (mouse) => Array.prototype.forEach.call(elementArray, (proxim) => proximityManager.effect( proxim, { x: mouse.x, y: mouse.y } )));
+	Array.prototype.forEach.call(elementArray, (proxim) => {
+		let proximityManager = new ProximityManager(proxim)
+		window.addEventListener("mousemove", (mouse) => proximityManager.effect({ x: mouse.x, y: mouse.y }));
+	})
 }
 export default listener
