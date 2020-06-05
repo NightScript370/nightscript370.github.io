3f023b2d7f4e4e27f0641c42b4e62e068d83f75e
NightScript <maorninja@outlook.com>
2020-06-05 13:13:53 -0700

    make effect more like Windows 10
    Credits to Evie11
    Todo: Optimization

--- assets/js/proximity.js
+++ assets/js/proximity.js
@@ -1,20 +1,23 @@
+const hoverradius = 300;
+const nohoverradius = 80;
+
 class ProximityManager {
 	constructor(element) {
 		this.proxim = element;
-		this.radius = 80;
 	}
 
 	effect(pos) {
 		let rect = this.proxim.getBoundingClientRect();
 		let x = pos.x - rect.left; // offset from corner
 		let y = pos.y - rect.top;
-		let color = getComputedStyle(document.documentElement).getPropertyValue("--hover-color");
-		let lighterColor = getComputedStyle(document.documentElement).getPropertyValue("--light-hover-color");
 
 		if (this.proxim.matches(":hover"))
-			this.proxim.style.background = lighterColor + ` radial-gradient( circle at ${x}px ${y}px, ${color} 0%, transparent ${this.radius}px)`;
+			this.proxim.style.background = `var(--light-hover-color) radial-gradient( circle at ${x}px ${y}px, var(--hover-color) 0%, transparent ${hoverradius}px)`;
 		else
-			this.proxim.style.background = `radial-gradient( circle at ${x}px ${y}px, ${lighterColor} 0%, transparent ${this.radius}px)`;
+			this.proxim.style.background = ``;
+
+		this.proxim.style.borderImageSource = `radial-gradient( circle at ${x}px ${y}px, var(--light-hover-color) 0%, transparent ${nohoverradius}px)`;
+		this.proxim.style.borderColor = "var(--hover-color)"
 	}
 }
 
