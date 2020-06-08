901e0e5c38de899f549b7a7b8babf9fdca9ddd2d
NightScript <maorninja@outlook.com>
2020-06-07 18:07:15 -0700

    Add in amoled theme

--- assets/js/sidebar/themes/index.js
+++ assets/js/sidebar/themes/index.js
@@ -2,6 +2,7 @@ export default async function () {
 	return {
 		'default': await import('./default.js'),
 		'discord': await import('./discord.js'),
+		'amoled': await import('./amoled.js'),
 		'light': await import('./light.js')
 	}
 }
