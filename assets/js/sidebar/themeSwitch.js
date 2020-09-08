1502a480a31dc0b1631f7433a439ea4eeff13990
Pk11 <epicpkmn11@outlook.com>
2020-09-08 16:28:03 -0500

    Switch themes to CSS files

--- assets/js/sidebar/themeSwitch.js
+++ assets/js/sidebar/themeSwitch.js
@@ -1,47 +1,42 @@
-import importThemes from './themes/index.js';
+if(!localStorage.theme)
+	localStorage.theme = "default";
 
-const themeSet = (key, theme) => {
-	for (const property in theme) {
-		if (theme.hasOwnProperty(property)) {
-			const element = theme[property];
-			document.documentElement.style.setProperty(property, element);
+Array.from(document.styleSheets).forEach(r => {
+	let theme = r.href.match(/assets\/css\/themes\/(.*?).css/);
+	if(theme)
+		theme = theme[1];
+	else
+		return;
+
+	let span = document.createElement("span");
+	span.setAttribute('data-tooltip', theme);
+	span.classList.add('themeDot')
+	span.classList.add('nstooltip')
+	span.style.background = r.cssRules[0].cssText.match(/--menu-element: *(.*?);/)[1];
+	span.onclick = () => themeSet(theme);
+	span.innerHTML = `<div data-tippy-content="${theme}" style="height: 100%; width: 100%;"></div>`;
+	document.getElementById("themesContainer").prepend(span);
+	console.log(theme)
+});
+
+function themeSet(theme) {
+	if(theme)
+		localStorage.theme = theme;
+	
+	Array.from(document.styleSheets).forEach(r => {
+		let theme = r.href.match(/assets\/css\/themes\/(.*?).css/);
+		if(theme)
+			theme = theme[1];
+		else
+			return;
+
+		if(theme == localStorage.theme) {
+			r.disabled = false;
+		} else {
+			r.disabled = true;
 		}
-	}
-
-	localStorage.setItem('theme', key);
+	});
 }
 
 if (!window.matchMedia("print").matches)
-	importThemes().then(themes => {
-		if (localStorage['theme']) {
-			if (themes[localStorage['theme']])
-				themeSet(localStorage['theme'], themes[localStorage['theme']].default)
-			else
-				localStorage['theme'] = 'default'
-		}
-	})
-
-export default async function setThemePicker() {
-	const themes = await importThemes();
-	const bottomLinks = document.getElementsByClassName('bottomLinks')[0];
-
-	const themesContainer = document.createElement("div");
-	for (const key in themes) {
-		if (!themes.hasOwnProperty(key))
-			continue;
-
-		const theme = themes[key].default;
-
-		let div = document.createElement("span");
-		div.setAttribute('data-tooltip', key);
-		div.classList.add('themeDot')
-		div.classList.add('nstooltip')
-		div.style.background = theme['--menu-element'];
-		div.addEventListener('click', () => themeSet(key, theme))
-		div.innerHTML = `<div data-tippy-content="${key}" style="height: 100%; width: 100%;"></div>`;
-
-		themesContainer.appendChild(div);
-	}
-
-	bottomLinks.insertAdjacentElement('afterbegin', themesContainer)
-}
+	themeSet();
