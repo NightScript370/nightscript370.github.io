e68a4be90dafccac75deaf53bf08ac3459f56807
Pk11 <epicpkmn11@outlook.com>
2020-09-08 16:42:35 -0500

    Add theme files
    Accidentally lost these when merging upstream

--- assets/js/sidebar/themes/index.js
+++
@@ -1,8 +0,0 @@
-export default async function () {
-	return {
-		'default': await import('./default.js'),
-		'discord': await import('./discord.js'),
-		'amoled': await import('./amoled.js'),
-		'light': await import('./light.js')
-	}
-}
